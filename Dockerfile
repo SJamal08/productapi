87101daa269e1d4bdf2bacbab756bb0e0e715df7
Jamal SIDIKOU <sidikjam08@gmail.com>
2021-02-09 17:21:34 +0100

    test ultime

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN npm install
 
 EXPOSE 3000
 
-CMD [ "node", "index.js" ]
+CMD [ "npm", "start" ]
--- mytests/routes.test.js
+++ mytests/routes.test.js
@@ -16,13 +16,23 @@ afterAll(async () => {
 
 describe('/api/products', () => {
 
-it('gets the test endpoint', async done => {
+it('get endpoint', async done => {
     const response = await request.get('/api/products')
   
     expect(response.status).toBe(200)
     done()
   })
 });
+
+describe('/api/products/id', () => {
+
+  it('get endpoint one id', async done => {
+      const response = await request.get('/api/products/6011b21ea9460847ac46a477')
+    
+      expect(response.status).toBe(200)
+      done()
+    })
+  });
 
 describe('Post Endpoints', () => {
     it('should create a new post', async done => {
